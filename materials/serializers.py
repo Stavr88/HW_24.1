9e30a29518a4772d3ef068f394478d1f8081fac9
Yura Sarvas <grey-matter88@mail.ru>
2024-10-24 00:05:00 +0300

    ДЗ 24.2

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,12 +1,18 @@
+from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
 
 from materials.models import Course, Lesson
 
 
 class CourseSerializer(ModelSerializer):
+    lesson_count = SerializerMethodField()
+
+    def get_lesson_count(self, object):
+        return object.lesson_set.count()
+
     class Meta:
         model = Course
-        fields = "__all__"
+        fields = ("name", "description", "image", "lesson_count")
 
 
 class LessonSerializer(ModelSerializer):
